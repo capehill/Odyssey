8c128b9f59159f60acb98f275b26866c6710fecd
capehill <juha.niemimaki@gmail.com>
2020-03-30 14:47:58 +0300

    Put back WritePixelArrayAlpha calls

--- odyssey-r155188-1.23/Source/WebKit/OrigynWebBrowser/Api/MorphOS/faviconclass.cpp
+++ odyssey-r155188-1.23/Source/WebKit/OrigynWebBrowser/Api/MorphOS/faviconclass.cpp
@@ -348,7 +348,7 @@ DEFMMETHOD(Draw)
 
 			if(src)
 			{
-				WritePixelArray((uint8 *) src, 0, 0, stride, PIXF_A8R8G8B8, _rp(obj), mleft, mtop, FAVICON_WIDTH*FACTOR, FAVICON_HEIGHT*FACTOR);
+				WritePixelArrayAlpha((APTR) src, 0, 0, stride, _rp(obj), mleft, mtop, FAVICON_WIDTH*FACTOR, FAVICON_HEIGHT*FACTOR, 0xffffffff);
 			}
 		}
 	}
